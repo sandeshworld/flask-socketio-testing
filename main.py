69b5843b818668a12c694eee1fe983095bb389f2
sandeshworld <sb3vf@virginia.edu>
2019-11-14 14:03:44 -0500

    adding flask view

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_socketio import SocketIO
+
+#create a flask object called app
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+
+#socketio is being applied to the app
+socketio = SocketIO(app)
+
+
+#we will use flask view
+
+#the following marker indicates the user visiting the home page.
+#which is indicated by the router decorator '/'
+@app.route('/')
+def sessions():
+	return render_template('session.html')
+
+
+
+#if another program is importing main.py then the if __name__ == '__main__' is
+#false, this helps seperate the executable part from the library part
+#when main.py is imported everything but the below is executed
+if __name__ = '__main__':
+	
+	socketio.run(app, debug=True) #the run method takes optional host
+	#and port arguments but by default will listen on localhost:5000.
+	#debug=True enables us to sort out the errors with ease
